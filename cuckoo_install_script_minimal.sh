5e1158ab4b46849a010c9681ba123b1593b310cc
Sreyas C.V <sreyas.codaw@gmail.com>
2024-01-20 23:41:25 -0600

    Code Cleaning

--- cuckoo_install_script_minimal.sh
+++ cuckoo_install_script_minimal.sh
@@ -1,11 +1,9 @@
 #!/bin/bash
 
-echo "\n[*] Updating System \n"
-sudo apt-get -y update
+echo "\n[*] This Script will skip certain commands. Cuckoo Analysis is not affected by skipped commands. Skipped commands are only those which automates certain things at reboot. \n"
 
-echo "\n[*]XXXXXXXXXXXXXX Creating user “cuckoo” and add them to sudo \n"
-# sudo adduser cuckoo
-# sudo adduser cuckoo sudo
+echo "\n[*] Updating System \n"
+sudo apt update && sudo apt upgrade -y
 
 echo "\n[*] Installing additional stuffs \n"
 sudo apt-get install -y python-dev libffi-dev libssl-dev libfuzzy-dev libtool flex autoconf libjansson-dev git curl python mongodb postgresql libpq-dev python-setuptools libjpeg-dev zlib1g-dev swig ssdeep net-tools
@@ -21,7 +19,7 @@ sudo python setup.py build
 sudo python setup.py install
 cd ..
 
-echo "\n[*] Installing additional python libs \n"
+echo "\n[*] Installing additional python libs (jupyter error will be raised, ignore it) \n"
 sudo -H pip install distorm3==3.4.4
 sudo -H pip install yara-python==3.6.3
 sudo -H pip install pydeep
@@ -33,7 +31,7 @@ echo "\n[*] Installing CUCKOO and creating default cuckoo workspace \n"
 sudo -H pip install -U cuckoo
 cuckoo
 
-echo "\n[*]XXXXXXXXXXXXXX Installing Virtual Box 6.1 \n"
+echo "\n[*]Installing Virtual Box 6.1 \n"
 curl https://www.virtualbox.org/download/oracle_vbox_2016.asc | gpg --dearmor > oracle_vbox_2016.gpg
 curl https://www.virtualbox.org/download/oracle_vbox.asc | gpg --dearmor > oracle_vbox.gpg
 sudo install -o root -g root -m 644 oracle_vbox_2016.gpg /etc/apt/trusted.gpg.d/
@@ -43,16 +41,9 @@ sudo apt update
 sudo apt install -y linux-headers-$(uname -r) dkms
 sudo apt install virtualbox-6.1 -y
 
-echo "\n[*]XXXXXXXXXXXXXX Creating and Configuring Host Only Network Adapter \n"
+echo "\n[*]Creating Host Only Network Adapter \n"
 vboxmanage hostonlyif create
 vboxmanage hostonlyif ipconfig vboxnet0 --ip 192.168.56.1 
-# sudo mkdir /opt/systemd/
-# printf "!/bin/bash\nhostonlyif create\nvboxmanage hostonlyif ipconfig vboxnet0 --ip 192.168.56.1" > /opt/systemd/vboxhostonly
-# sudo chmod a+x /opt/systemd/vboxhostonly
-# printf "Description=Setup VirtualBox Hostonly Adapter\nAfter=vboxdrv.service\n[Service]\nType=oneshot\nExecStart=/opt/systemd/vboxhostonly\n[Install]\nWantedBy=multi-user.target" > /etc/systemd/system/vboxhostonlynic.service
-# sudo systemctl daemon-reload
-# sudo systemctl enable vboxhostonlynic.service
-# sudo systemctl start vboxhostonlynic.service
 
 echo "\n[*] Configuring Internet for vboxnet0 \n"
 sudo apt install -y iptables-persistent
@@ -62,9 +53,9 @@ nic_interface_name=$(ip -o -4 route show to default | awk '{print $5}')
 sudo iptables -A FORWARD -o $nic_interface_name -i vboxnet0 -s 192.168.56.0/24 -m conntrack --ctstate NEW -j ACCEPT
 sudo iptables -A FORWARD -m conntrack --ctstate ESTABLISHED,RELATED -j ACCEPT
 sudo iptables -t nat -A POSTROUTING -o $nic_interface_name -j MASQUERADE
-
+ 
 echo 1 | sudo tee -a /proc/sys/net/ipv4/ip_forward
 sudo sysctl -w net.ipv4.ip_forward=1
 sudo su -c "iptables-save > /etc/iptables/rules.v4"
 
-echo "\n[*] The script have executed successfully. Now you have to install Windows 7 in virtualbox VM and edit cuckoo configuration files. Please have a look at the installation notes.\n"
+echo "\n[*] The script have executed successfully. Now you have to install Windows 7 in virtualbox VM and edit cuckoo configuration files. Please have a look at the installation notes.\n" 
